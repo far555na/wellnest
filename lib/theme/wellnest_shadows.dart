385db5d51418b9bbdea783c529a2334e836c2ad5
far555na <farhana.malem@gmail.com>
2026-06-05 14:38:01 +0700

    chore: add shadow style

---
+++ lib/theme/wellnest_shadows.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WellnestShadows {
+  WellnestShadows._();
+
+  static const List<BoxShadow> boxShadow = [
+    BoxShadow(
+      color: Color(0xFFEFEDED),
+      offset: Offset(0, 8),
+      blurRadius: 24,
+      spreadRadius: 0,
+    ),
+  ];
+}
